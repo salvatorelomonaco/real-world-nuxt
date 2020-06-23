8eaee5357e5eded666ed45a66aaafeed7c3923f0
salvatorelomonaco <lommetto95@gmail.com>
2020-06-23 22:00:04 +0200

    Pagina di errore - Dynamic Route

---
+++ pages/event/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- usare _ comunica a Nuxt di creare un segmento dinamico -->
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+  // proprieta' usata da vue-meta
+  head() {
+    return {
+      // titolo per la pagina
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          // descrizione per la pagina
+          hid: 'description',
+          name: 'description',
+          content: 'What you need to know about event #' + this.id,
+        },
+      ],
+    }
+  },
+}
+</script>
